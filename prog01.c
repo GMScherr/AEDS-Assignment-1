ae5d73fe9c6ae4c960aa7edf979ead7e30bc74d6
GMScherr <54864203+GMScherr@users.noreply.github.com>
2020-03-03 17:59:30 -0300

    Add files via upload

--- prog01.c
+++ prog01.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 typedef struct road {
 	char departureLocation[20];
 	char arrivalLocation[20];
 	int distance;
 } road;
+
+int vertexCount (road *roadList,int roadAmount){//Uses the list of roads in order to count the amount of vertices/cities in the graph
+	char currentString[20];
+	int vertexNum = 1;
+	for (int i = 0;i<20;i++)
+		currentString[i]=roadList[0].departureLocation[i];//Copies the first city in the list into currentString
+	for (int i = 0;i<roadAmount;i++)
+		if (strcmp(roadList[i].departureLocation,currentString) != 0){//If the name of the departure location is different fromt the current name, a new city name has been found
+			vertexNum++;                                              //Note : this only works because of how the input file is structured, this is *not* a proper sorter and counter
+			for (int j = 0;j<20;j++)
+				currentString[j]=roadList[i].departureLocation[j];
+		}
+	return vertexNum;
+}
 
 int main(){
 	//File opening
@@ -31,7 +46,7 @@ int main(){
 		i--;
 		j++;
 	}
-	roadAmount = roadAmount/10;printf("%d\n",roadAmount);//Note to self : roadAmount is the expected result times 10. I'll come back to solve it later, for now, this will have to do
+	roadAmount = roadAmount/10;//Note to self : roadAmount is the expected result times 10. I'll come back to solve it later, for now, this will have to do
 	i=0;j=0;//Variables i and j are reset in the end of every block where they are needed, for the sake of posterity
 	//Code responsible for reading the remainder of the lines in the input file
 	road roadList[roadAmount];
@@ -55,4 +70,7 @@ int main(){
 	printf("There are %d roads in this map\n",roadAmount );*/ //Commented code is used to prove that the program has successfully transfered all the info from the file to the struct array
 	i=0;j=0;
 	fclose(inputFile);//File is no longer needed so it may be closed
+
+	int vertex = vertexCount(roadList,roadAmount);//Amount of vertices in the graph
+	printf("%d\n",vertex );
 }
