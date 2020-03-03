0e39ef32b35bb69723687365bfab992ff2ca70fe
GMScherr <54864203+GMScherr@users.noreply.github.com>
2020-03-03 17:09:26 -0300

    Add files via upload

--- prog01.c
+++ prog01.c
@@ -20,25 +20,39 @@ int main(){
 	}
 	//Code necessary to read the very first line of the input file. At the time of writing all this code I had no idea it could be done with a simple fscanf. Bollocks
 	int roadAmount = 0;
-	char currentString[100];
+	char currentString[100];//Temporary storage space for strings
 	fgets (currentString,100,inputFile);
 	int i = 0;//Auxiliary number 01
 	int j = 0;//Auxiliary number 02
 	while (currentString[i] != '\\')
 		i++;
-	printf("%d\n",i);
 	while (i>0){
 		roadAmount = roadAmount + (currentString[j]-48)*pow(10,i);
 		i--;
 		j++;
 	}
-	roadAmount = roadAmount/10;//Note to self : roadAmount is the expected result times 10. I'll come back to solve it later, for now, this will have to do
+	roadAmount = roadAmount/10;printf("%d\n",roadAmount);//Note to self : roadAmount is the expected result times 10. I'll come back to solve it later, for now, this will have to do
 	i=0;j=0;//Variables i and j are reset in the end of every block where they are needed, for the sake of posterity
 	//Code responsible for reading the remainder of the lines in the input file
 	road roadList[roadAmount];
+	for (i=0;i<roadAmount;i++)
+		roadList[i].distance = 0;
 	for (i=0;i<roadAmount;i++){
-		fgets (currentString,100,inputFile);
+		fscanf(inputFile,"%s %s %s", roadList[i].departureLocation , roadList[i].arrivalLocation , currentString);
+		int aux = 0; //Aux int used to turn the String that determines distance into an int
+		while (currentString[j] != '\\')
+			j++;
+		while (j>0){
+			roadList[i].distance = roadList[i].distance + (currentString[aux]-48)*pow(10,j);
+			j--;
+			aux++;
+		}
+		roadList[i].distance = roadList[i].distance/10;//The code to turn String to int was essentially copied from above and suffers from the same issue. Note to self : might as well turn it into a function
 	}
 
-	fclose(inputFile);
+	/*for (i = 0 ; i<roadAmount ; i++)
+		printf("%s %s %d\n",roadList[i].departureLocation,roadList[i].arrivalLocation,roadList[i].distance );
+	printf("There are %d roads in this map\n",roadAmount );*/ //Commented code is used to prove that the program has successfully transfered all the info from the file to the struct array
+	i=0;j=0;
+	fclose(inputFile);//File is no longer needed so it may be closed
 }
